65c2a37f660d45d75f11ea979f65b8cbb8dc2834
Brian Mwasi <>
2020-08-18 12:53:40 +0300

    Updated R script to include available work time

--- index.R
+++ index.R
@@ -1,44 +1,62 @@
+PackagesList <- c("readxl", "openxlsx","sjmisc", "tidyverse")
+Packages <- PackagesList[!(PackagesList %in% installed.packages()[,"Package"])]
+if(length(Packages)) install.packages(Packages)
+
 library(readxl)
-library(openxlsx)
+library(sjmisc)
+# library(openxlsx)
 
 # Merge data collection templates 
-path <- "HRH Data Collection"
-master_file_name <- "HRH Data Collection Template_Master.xlsm"
-
-data_collection_template_list <- list.files(path= path, full.names=TRUE)
-
-All <- lapply(data_collection_template_list,function(filename){
-  print(paste("Merging",filename,sep = " "))
-  read.xlsx(filename)
-})
-
-df <- do.call(rbind.data.frame, All)
-write.xlsx(df,master_file_name)
+# path <- "HRH Data Collection"
+# master_file_name <- "HRH Data Collection Template_Master.xlsm"
+# 
+# data_collection_template_list <- list.files(path= path, full.names=TRUE)
+# 
+# All <- lapply(data_collection_template_list,function(filename){
+#   print(paste("Merging",filename,sep = " "))
+#   read.xlsx(filename)
+# })
+# 
+# df <- do.call(rbind.data.frame, All)
+# write.xlsx(df,master_file_name)
 
 #Import data from master file
-import_location <- "HRH Data Collection Template_Master.xlsm"
-# Import File ID and Ref
-FileIdRef <- read_excel(import_location, 
-                        sheet = "1. File ID and Ref")
+import_location <- "HRH Data Collection/HRH Data Collection Template_Country Name.xlsm"
+# Import PSNU List
+PSNUList <- read_excel(import_location, 
+                       sheet = "1. PSNU List")
 # Import Program targets for COP year
 ProgramTargets <- read_excel(import_location, 
-                             sheet = "2. Program targets for COP year")
+                             sheet = "2. Program targets for COP year",
+                             skip = 1)
 # Import PEPFAR Target Attribution
 TargetAttribution <- read_excel(import_location, 
-                             sheet = "3. PEPFAR Target Atribution")
+                                sheet = "3. PEPFAR Target Attribution",
+                                skip = 1)
 # Import HIV Services Time Allocation
 HIVServicesTime <- read_excel(import_location, 
-                                sheet = "4. HIV Services Time Allocation")
+                              sheet = "4. HIV Services Time Allocation",
+                              skip = 1)
 # Import Client Pathways
 ClientPathways <- read_excel(import_location, 
-                             sheet = "5. Client Pathways", range = "A2:B21", 
-                             col_names = FALSE)
+                             sheet = "5. Client Pathways", range = "C1:D76")
+ClientPathways <- rotate_df(ClientPathways, cn = TRUE)
 # Import Current PEPFAR HRH
 CurrentHRH <- read_excel(import_location, 
-                        sheet = "6. Current PEPFAR HRH")
+                         sheet = "6. Current PEPFAR HRH",
+                         skip = 1)
 # Import Current HRH Salaries
 CurrentSalaries <- read_excel(import_location, 
-                             sheet = "7. Current HRH Salaries")
+                              sheet = "7. Current HRH Salaries",
+                              skip = 1)
 # Import Program Budgets
-CurrentSalaries <- read_excel(import_location, 
-                              sheet = "8. Program Budgets")
+ProgramBudgets <- read_excel(import_location, 
+                             sheet = "8. Program Budgets", range = "A2:E3")
+# Available working time
+AvailableWorkingTime <- read_excel(import_location, 
+                                   sheet = "9. Available Working Time", 
+                                  range = "B4:I12")
+# Default non-clinical work hours per week
+WeeklyNonClinicalWorkingHours <- read_excel(import_location, 
+                                            sheet = "9. Available Working Time", 
+                                            range = "B17:C25")
